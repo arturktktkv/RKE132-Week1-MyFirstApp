ecb84517dc983934bf80ff48cfeceea6d2304074
artur <artur.kotov@tktk.ee>
2024-12-18 22:45:11 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// This is comment to my code
+
+
+//rakendus küsib kasutajalt nime sisestamist
+//rakendus tervitab kasutajat nimepidi
+Console.WriteLine("Enter your name:");//Output
+//string- sõne
+string userName = Console.ReadLine();//Input
+
+Console.WriteLine("Hello" + ", " + userName + "!");//Output
+//string interpolation
+Console.WriteLine($"Hello, {userName}!");//Output
